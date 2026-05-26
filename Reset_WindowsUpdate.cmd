324484ddd4bf842f1d6b291a150ef141d9f7d913
MatBanik <banikm@gmail.com>
2026-05-26 11:57:42 -0400

    bump: v1.0.7

--- Reset_WindowsUpdate.cmd
+++ Reset_WindowsUpdate.cmd
@@ -15,7 +15,7 @@ setlocal EnableDelayedExpansion
 :: ============================================================
 
 :: Version: bump this before each GitHub release (semver: MAJOR.MINOR.PATCH)
-set "ver=1.0.6"
+set "ver=1.0.7"
 
 :: ============================================================
 :: OPTIONS (change these before running if needed)
